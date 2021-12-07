3d03e00a059e2bed2e77d4942159e3acba81c843
Zakisu <zaki.sulistya@gmail.com>
2021-12-07 20:29:25 +0700

    First Update

--- README.md
+++ README.md
@@ -1,61 +1,71 @@
-# OpenCart Module Template Generator
 
-Create a basic template for Opencart module using Robo
+# Opencart Module Template Generator
 
+Create a basic template for Opencart module using Robo. This repo modified from [Opencart Module Generator by Nursan Amar](https://github.com/nursanamar/OpenCart-module-generator)
+## Note
+
+    Requirement: PHP 7.x
 ## Installation
 
-```bash
 
-#Clone the repository
-$ git clone https://github.com/nursanamar/OpenCart-module-generator.git
-$ cd OpenCart-module-generator
-
-#Install dependecies
-$ composer install
-
-#.env file
-$ cp .env.example .env
-
-```
+```bash  
+  
+#Clone the repository  
+$ git clone https://github.com/zakisu/opencart-module-generator.git  
+$ cd opencart-module-generator  
+  
+#Install dependecies  
+$ composer install  
+  
+#.env file  
+$ cp .env.example .env  
+  
+```  
 
 Setup your `.env` file
+### Example .ENV Setting
+
+    OC_ROOT="/var/www/html/oc37"  
+    MODULE_NAME="Anon Module"  
+    MODULE_VER="1.0.0"  
+    YAKPRO_PO_CONFIG_FILE="/usr/local/yakpro-po/yakpro-po.cnf"  
+    STORE_DOMAIN="your-web-site.com"  
+    STORE_USERNAME="admin"  
+    STORE_PASSWORD="admin"
+
 
 ## Usage
 
 ### Create new module
 
-```bash
-$ vendor/bin/robo module:new
-```
-Generate controller,model,view,etc files in `src`
-
-### Install module
-
-```bash
-$ vendor/bin/robo module:install
-```
-Copy all file form `src/upload` into your Opencart directory (from your .env file)
-
-### Watch module
-
-```bash
-$ vendor/bin/robo module:watch
-```
-
-Wacth any changes in `src/upload` and copy changed files into Opencart directory (from your .env file)
-
-### Build module
-
-```bash
+```bash  
+$ vendor/bin/robo module:new```  
+Generate controller,model,view,etc files in `src`  
+  
+### Install module  
+  
+```bash  
+$ vendor/bin/robo module:install```  
+Copy all file form `src/upload` into your Opencart directory (from your .env file)  
+  
+### Watch module  
+  
+```bash  
+$ vendor/bin/robo module:watch```  
+  
+Wacth any changes in `src/upload` and copy changed files into Opencart directory (from your .env file)  
+  
+### Build module  
+  
+```bash  
 $ vendor/bin/robo module:build
-```
+```  
 
 Generate ocmod file in `build` folder, add `--with-obf` option to build the obfuscated version of your ocmod
 
 ### Deploy module
 
-```bash
-$ vendor/bin/robo module:deploy
-```
-
+```bash  
+$ vendor/bin/robo module:deploy```  
+  
 Upload generated ocmod file to the store
--- RoboFile.php
+++ RoboFile.php
@@ -8,332 +8,335 @@
 require_once 'vendor/autoload.php';
 
 if (file_exists('.env')) {
-    $dotenv = Dotenv\Dotenv::create(__DIR__);
-    $dotenv->load();
+	$dotenv = Dotenv\Dotenv::create(__DIR__);
+	$dotenv->load();
 }
 
 use pmaslak\PhpObfuscator\Obfuscator;
 
 class RoboFile extends \Robo\Tasks
 {
-    public function moduleNew()
-    {
-        $name = $this->ask("Module Name");
+	public function moduleNew()
+	{
+		$name = $this->ask("Module Name");
 
-        $filename = str_replace(' ', '_', strtolower($name));
-        $path = "extension/module";
+		$filename = str_replace(' ', '_', strtolower($name));
+		$path = "extension/module";
 
-        $this->makeController($path, $filename);
-        $this->makeModel($path, $filename);
-        $this->makeTwig($path, $filename);
+		$this->makeController($path, $filename);
+		$this->makeModel($path, $filename);
+		$this->makeTwig($path, $filename);
 
-        $language_file = $this->makeLanguage($path, $filename);
+		$language_file = $this->makeLanguage($path, $filename);
 
-        $this->taskWriteToFile($language_file)
-            ->line("<?php\n")
-            ->line('$_["heading_title"] = "' . $name . '";')
-            ->run();
-    }
+		$this->taskWriteToFile($language_file)
+			->line("<?php\n")
+			->line('$_["heading_title"] = "' . $name . '";')
+			->run();
+	}
 
-    private function makeController($path, $filename)
-    {
-        $pathPart = explode("/", $path);
+	private function makeController($path, $filename)
+	{
+		$pathPart = explode("/", $path);
 
-        $class_name = "Controller" . str_replace(" ", "", ucwords(implode(" ", $pathPart)));
-        $class_name .= str_replace(" ", '', ucwords(str_replace('_', ' ', $filename)));
+		$class_name = "Controller" . str_replace(" ", "", ucwords(implode(" ", $pathPart)));
+		$class_name .= str_replace(" ", '', ucwords(str_replace('_', ' ', $filename)));
 
-        $dir = getenv('MODULE_FOLDER') . "/upload/admin/controller/" . $path . "/";
-        $file_path = $dir . $filename . ".php";
+		$dir = getcwd() . "/upload/admin/controller/" . $path . "/";
+		$file_path = $dir . $filename . ".php";
 
-        $this->_mkdir($dir);
-        $this->_touch($file_path);
+		$this->_mkdir($dir);
+		$this->_touch($file_path);
 
-        $this->say($class_name);
-        $this->taskWriteToFile($file_path)
-            ->line("<?php\n")
-            ->line("class " . $class_name . " extends Controller {\n")
-            ->line("\tpublic function index(){\n")
-            ->line("\t}")
-            ->line("}\n")
-            ->run();
+		$this->say($class_name);
+		$this->taskWriteToFile($file_path)
+			->line("<?php\n")
+			->line("class " . $class_name . " extends Controller {\n")
+			->line("\tpublic function index(){\n")
+			->line("\t}")
+			->line("}\n")
+			->run();
 
-        $dir = getenv('MODULE_FOLDER') . "/upload/catalog/controller/" . $path . "/";
-        $file_path = $dir . $filename . ".php";
+		$dir = getcwd() . "/upload/catalog/controller/" . $path . "/";
+		$file_path = $dir . $filename . ".php";
 
-        $this->_mkdir($dir);
-        $this->_touch($file_path);
+		$this->_mkdir($dir);
+		$this->_touch($file_path);
 
-        $this->say($class_name);
-        $this->taskWriteToFile($file_path)
-            ->line("<?php\n")
-            ->line("class " . $class_name . " extends Controller {\n")
-            ->line("\tpublic function index(){\n")
-            ->line("\t}")
-            ->line("}\n")
-            ->run();
-    }
+		$this->say($class_name);
+		$this->taskWriteToFile($file_path)
+			->line("<?php\n")
+			->line("class " . $class_name . " extends Controller {\n")
+			->line("\tpublic function index(){\n")
+			->line("\t}")
+			->line("}\n")
+			->run();
+	}
 
-    private function makeModel($path, $filename)
-    {
-        $pathPart = explode("/", $path);
+	private function makeModel($path, $filename)
+	{
+		$pathPart = explode("/", $path);
 
-        $class_name = "Model" . str_replace(" ", "", ucwords(implode(" ", $pathPart)));
-        $class_name .= str_replace(" ", '', ucwords(str_replace('_', ' ', $filename)));
+		$class_name = "Model" . str_replace(" ", "", ucwords(implode(" ", $pathPart)));
+		$class_name .= str_replace(" ", '', ucwords(str_replace('_', ' ', $filename)));
 
-        $dir = getenv('MODULE_FOLDER') . "/upload/admin/model/" . $path . "/";
-        $file_path = $dir . $filename . ".php";
+		$dir = getcwd() . "/upload/admin/model/" . $path . "/";
+		$file_path = $dir . $filename . ".php";
 
-        $this->_mkdir($dir);
-        $this->_touch($file_path);
+		$this->_mkdir($dir);
+		$this->_touch($file_path);
 
-        $this->say($class_name);
-        $this->taskWriteToFile($file_path)
-            ->line("<?php\n")
-            ->line("class " . $class_name . " extends Model {\n\n")
-            ->line("}\n")
-            ->run();
-    }
+		$this->say($class_name);
+		$this->taskWriteToFile($file_path)
+			->line("<?php\n")
+			->line("class " . $class_name . " extends Model {\n\n")
+			->line("}\n")
+			->run();
+	}
 
-    private function makeTwig($path, $filename)
-    {
-        $dir = getenv('MODULE_FOLDER') . "/upload/admin/view/template/" . $path . "/";
-        $file_path = $dir . $filename . ".twig";
+	private function makeTwig($path, $filename)
+	{
+		$dir = getcwd() . "/upload/admin/view/template/" . $path . "/";
+		$file_path = $dir . $filename . ".twig";
 
-        $this->_mkdir($dir);
-        $this->_touch($file_path);
+		$this->_mkdir($dir);
+		$this->_touch($file_path);
 
-        $dir = getenv('MODULE_FOLDER') . "/upload/catalog/view/theme/default/template/" . $path . "/";
-        $file_path = $dir . $filename . ".twig";
+		$dir = getcwd() . "/upload/catalog/view/theme/default/template/" . $path . "/";
+		$file_path = $dir . $filename . ".twig";
 
-        $this->_mkdir($dir);
-        $this->_touch($file_path);
-    }
+		$this->_mkdir($dir);
+		$this->_touch($file_path);
+	}
 
-    private function makeLanguage($path, $filename, $code = 'en-gb')
-    {
+	private function makeLanguage($path, $filename, $code = 'en-gb')
+	{
 
-        $dir = getenv('MODULE_FOLDER') . '/upload/admin/language/' . $code . '/' . $path . '/';
-        $file_path = $dir . $filename . ".php";
+		$dir = getcwd() . '/upload/admin/language/' . $code . '/' . $path . '/';
+		$file_path = $dir . $filename . ".php";
 
-        $this->_mkdir($dir);
-        $this->_touch($file_path);
+		$this->_mkdir($dir);
+		$this->_touch($file_path);
 
-        return $file_path;
-    }
+		return $file_path;
+	}
 
-    public function moduleInstall()
-    {
-        $this->taskFilesystemStack()
-            ->mirror(getenv('MODULE_FOLDER') . '/upload', getenv('OC_ROOT'))
-            ->copy(getenv('MODULE_FOLDER') . '/install.xml', getenv('OC_ROOT') . '/system/install.ocmod.xml')
-            ->run();
-    }
+	public function moduleInstall()
+	{
+		$this->taskFilesystemStack()
+			->mirror(getcwd() . '/upload', getenv('OC_ROOT'))
+			->copy(getcwd() . '/install.xml', getenv('OC_ROOT') . '/system/install.ocmod.xml')
+			->run();
+	}
 
-    public function moduleWatch()
-    {
-        $this->moduleInstall();
+	public function moduleWatch()
+	{
+		$this->moduleInstall();
 
-        $this->taskWatch()->monitor(getenv("MODULE_FOLDER"), function () {
-            $this->moduleInstall();
-        })->run();
-    }
+		$this->taskWatch()->monitor(getcwd(), function () {
+			$this->moduleInstall();
+		})->run();
+	}
 
-    public function moduleBuild($opts = ["with-obf" => false])
-    {
-        $this->taskDeleteDir('build')->run();
-        $this->taskFileSystemStack()->mkdir('build')->run();
+	public function moduleBuild($opts = ["with-obf" => false])
+	{
+		$dir = getcwd() . '/build';
+		$this->taskDeleteDir($dir)->run();
+		$this->taskFileSystemStack()->mkdir($dir)->run();
 
-        $module_name = getenv("MODULE_NAME") ? getenv("MODULE_NAME") : "build";
-        $ver = getenv("MODULE_VER") ? "v" . getenv("MODULE_VER") : '';
-        $filename = $module_name . $ver . '.ocmod.zip';
-        $this->taskExec('zip')->dir(getenv("MODULE_FOLDER"))->arg('-r')->arg('../build/' . $filename)->arg('./')->run();
+		$module_name = getenv("MODULE_NAME") ? getenv("MODULE_NAME") : "build";
+		$ver = getenv("MODULE_VER") ? "v" . getenv("MODULE_VER") : '';
+		$filename = $module_name . $ver . '.ocmod.zip';
+		$this->taskExec('zip')->dir(getcwd())->arg('-r')->arg($dir . '/' . $filename)->arg('./')->run();
 
-        if ($opts['with-obf']) {
-            $this->moduleObf();
-        }
-    }
+		if ($opts['with-obf']) {
+			$this->moduleObf();
+		}
+	}
 
-    public function moduleObf()
-    {
-        $this->taskDeleteDir('obf')->run();
-        $this->taskFileSystemStack()->mkdir('obf')->run();
+	public function moduleObf()
+	{
+		$dir = getcwd() . '/obf';
+		
+		$this->taskDeleteDir($dir)->run();
+		$this->taskFileSystemStack()->mkdir($dir)->run();
 
-        $options = [
-            "no-obfuscate-method-name",
-            "no-obfuscate-class-name",
-            "no-obfuscate-property-name",
-            "no-obfuscate-constant-name",
-            "no-obfuscate-variable-name",
-        ];
+		$options = [
+			"no-obfuscate-method-name",
+			"no-obfuscate-class-name",
+			"no-obfuscate-property-name",
+			"no-obfuscate-constant-name",
+			"no-obfuscate-variable-name",
+		];
 
-        $obfuscator = new Obfuscator([
-            "allowed_mime_types" => ['text/x-php'],
-            "obfuscation_options" => $options,
-        ]);
+		$obfuscator = new Obfuscator([
+			"allowed_mime_types" => ['text/x-php'],
+			"obfuscation_options" => $options,
+		]);
 
-        $obfuscator->obfuscateDirectory(getenv("MODULE_FOLDER") . '/', 'obf/');
+		$obfuscator->obfuscateDirectory(getcwd() . '/upload/', $dir . '/');
 
-        $this->taskFilesystemStack()->copy(getenv("MODULE_FOLDER") . '/install.xml', 'obf/install.xml')->run();
+		$this->taskFilesystemStack()->copy(getcwd() . '/install.xml', $dir. '/install.xml')->run();
 
-        $module_name = getenv("MODULE_NAME") ? getenv("MODULE_NAME") : "build";
-        $ver = getenv("MODULE_VER") ? "v" . getenv("MODULE_VER") : '';
-        $filename = $module_name . $ver . '-obf.ocmod.zip';
-        $this->taskExec('zip')->dir('obf')->arg('-r')->arg('../build/' . $filename)->arg('./')->run();
+		$module_name = getenv("MODULE_NAME") ? getenv("MODULE_NAME") : "build";
+		$ver = getenv("MODULE_VER") ? "v" . getenv("MODULE_VER") : '';
+		$filename = $module_name . $ver . '-obf.ocmod.zip';
+		$this->taskExec('zip')->dir($dir)->arg('-r')->arg(getcwd() . '/build/' . $filename)->arg('./')->run();
 
-        $this->taskDeleteDir('obf')->run();
-    }
+		$this->taskDeleteDir($dir)->run();
+	}
 
-    public function moduleDeploy()
-    {
-        $storeDomain = getenv("STORE_DOMAIN");
-        $account = [
-            "username" => getenv("STORE_USERNAME"),
-            "password" => getenv("STORE_PASSWORD"),
-        ];
+	public function moduleDeploy()
+	{
+		$storeDomain = getenv("STORE_DOMAIN");
+		$account = [
+			"username" => getenv("STORE_USERNAME"),
+			"password" => getenv("STORE_PASSWORD"),
+		];
 
-        $this->io()->title("Start deploying module to " . $storeDomain);
+		$this->io()->title("Start deploying module to " . $storeDomain);
 
-        $this->io()->section("Build the module");
-        $this->moduleBuild();
+		$this->io()->section("Build the module");
+		$this->moduleBuild();
 
-        $this->io()->section("Connect to " . $storeDomain);
-        $store = new Store($storeDomain, $account);
+		$this->io()->section("Connect to " . $storeDomain);
+		$store = new Store($storeDomain, $account);
 
-        $module_name = getenv("MODULE_NAME") ? getenv("MODULE_NAME") : "build";
-        $ver = getenv("MODULE_VER") ? "v" . getenv("MODULE_VER") : '';
-        $filename = $module_name . $ver . '.ocmod.zip';
+		$module_name = getenv("MODULE_NAME") ? getenv("MODULE_NAME") : "build";
+		$ver = getenv("MODULE_VER") ? "v" . getenv("MODULE_VER") : '';
+		$filename = $module_name . $ver . '.ocmod.zip';
 
-        $this->io()->section("Upload " . $filename);
-        $store->uploadModule("build/" . $filename);
+		$this->io()->section("Upload " . $filename);
+		$store->uploadModule("build/" . $filename);
 
-        $this->io()->section("Refresh store modification");
-        $store->refreshModification();
+		$this->io()->section("Refresh store modification");
+		$store->refreshModification();
 
-        $this->io()->success("Done, module installed on " . $storeDomain);
+		$this->io()->success("Done, module installed on " . $storeDomain);
 
-    }
+	}
 }
 
 class Store
 {
 
-    private $ch;
-    private $domain;
-    private $adminAccount;
-    private $user_token = null;
-    private $path = [
-        "login" => "common/login",
-        "modification_refresh" => "marketplace/modification/refresh",
-        "upload_module" => "marketplace/installer/upload",
-    ];
+	private $ch;
+	private $domain;
+	private $adminAccount;
+	private $user_token = null;
+	private $path = [
+		"login" => "common/login",
+		"modification_refresh" => "marketplace/modification/refresh",
+		"upload_module" => "marketplace/installer/upload",
+	];
 
-    public function __construct($domain, $adminAccount, $routes = [])
-    {
+	public function __construct($domain, $adminAccount, $routes = [])
+	{
 
-        $this->domain = $domain;
-        $this->adminAccount = $adminAccount;
-        $this->path = array_merge($this->path, $routes);
+		$this->domain = $domain;
+		$this->adminAccount = $adminAccount;
+		$this->path = array_merge($this->path, $routes);
 
-        $ch = curl_init();
-        curl_setopt_array($ch, array(
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_COOKIEJAR => dirname(__FILE__) . '/cookie.txt',
-            CURLOPT_COOKIEFILE => dirname(__FILE__) . '/cookie.txt',
-            CURLOPT_VERBOSE => true,
-        ));
+		$ch = curl_init();
+		curl_setopt_array($ch, [
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => "",
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 30,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_COOKIEJAR => dirname(__FILE__) . '/cookie.txt',
+			CURLOPT_COOKIEFILE => dirname(__FILE__) . '/cookie.txt',
+			CURLOPT_VERBOSE => true,
+		]);
 
-        $verbose = fopen(dirname(__FILE__) . '/errorlog.txt', 'w');
-        curl_setopt($ch, CURLOPT_STDERR, $verbose);
+		$verbose = fopen(dirname(__FILE__) . '/errorlog.txt', 'w');
+		curl_setopt($ch, CURLOPT_STDERR, $verbose);
 
-        $this->ch = $ch;
+		$this->ch = $ch;
 
-        $this->login();
-    }
+		$this->login();
+	}
 
-    public function login()
-    {
-        curl_setopt_array($this->ch, [
-            CURLOPT_URL => $this->domain . "admin/index.php?route=" . $this->path['login'],
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => http_build_query($this->adminAccount),
-        ]);
+	public function login()
+	{
+		curl_setopt_array($this->ch, [
+			CURLOPT_URL => $this->domain . "admin/index.php?route=" . $this->path['login'],
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => http_build_query($this->adminAccount),
+		]);
 
-        $response = curl_exec($this->ch);
-        $url_info = curl_getinfo($this->ch, CURLINFO_EFFECTIVE_URL);
+		$response = curl_exec($this->ch);
+		$url_info = curl_getinfo($this->ch, CURLINFO_EFFECTIVE_URL);
 
-        $url = parse_url($url_info, PHP_URL_QUERY);
-        parse_str($url, $query);
+		$url = parse_url($url_info, PHP_URL_QUERY);
+		parse_str($url, $query);
 
-        if (isset($query['user_token'])) {
-            $this->user_token = $query['user_token'];
-        }
-    }
+		if (isset($query['user_token'])) {
+			$this->user_token = $query['user_token'];
+		}
+	}
 
-    public function refreshModification()
-    {
-        curl_setopt_array($this->ch, [
-            CURLOPT_URL => $this->domain . "admin/index.php?route=" . $this->path['modification_refresh'] . "&user_token=" . $this->user_token,
-            CURLOPT_CUSTOMREQUEST => "GET",
-        ]);
+	public function refreshModification()
+	{
+		curl_setopt_array($this->ch, [
+			CURLOPT_URL => $this->domain . "admin/index.php?route=" . $this->path['modification_refresh'] . "&user_token=" . $this->user_token,
+			CURLOPT_CUSTOMREQUEST => "GET",
+		]);
 
-        $response = curl_exec($this->ch);
-        $url_info = curl_getinfo($this->ch, CURLINFO_EFFECTIVE_URL);
+		$response = curl_exec($this->ch);
+		$url_info = curl_getinfo($this->ch, CURLINFO_EFFECTIVE_URL);
 
-        $url = parse_url($url_info, PHP_URL_QUERY);
-        parse_str($url, $query);
+		$url = parse_url($url_info, PHP_URL_QUERY);
+		parse_str($url, $query);
 
-    }
+	}
 
-    public function uploadModule($modulePath)
-    {
-        $file = [
-            'file' => new CurlFile($modulePath, 'application/zip', basename($modulePath)),
-        ];
+	public function uploadModule($modulePath)
+	{
+		$file = [
+			'file' => new CurlFile($modulePath, 'application/zip', basename($modulePath)),
+		];
 
-        curl_setopt_array($this->ch, [
-            CURLOPT_URL => $this->domain . "admin/index.php?route=" . $this->path['upload_module'] . "&user_token=" . $this->user_token,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => $file,
-        ]);
+		curl_setopt_array($this->ch, [
+			CURLOPT_URL => $this->domain . "admin/index.php?route=" . $this->path['upload_module'] . "&user_token=" . $this->user_token,
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => $file,
+		]);
 
-        $response = curl_exec($this->ch);
+		$response = curl_exec($this->ch);
 
-        $response = json_decode($response, true);
+		$response = json_decode($response, true);
 
-        print($response['text'] . "\n");
-        $this->next($response['next']);
-    }
+		print($response['text'] . "\n");
+		$this->next($response['next']);
+	}
 
-    public function next($url)
-    {
-        curl_setopt_array($this->ch, [
-            CURLOPT_URL => $url,
-            CURLOPT_CUSTOMREQUEST => "GET",
-        ]);
+	public function next($url)
+	{
+		curl_setopt_array($this->ch, [
+			CURLOPT_URL => $url,
+			CURLOPT_CUSTOMREQUEST => "GET",
+		]);
 
-        $response = curl_exec($this->ch);
+		$response = curl_exec($this->ch);
 
-        $response = json_decode($response, true);
+		$response = json_decode($response, true);
 
-        if (isset($response['text'])) {
-            print($response['text'] . "\n");
-        }
+		if (isset($response['text'])) {
+			print($response['text'] . "\n");
+		}
 
-        if (isset($response['next'])) {
-            $this->next($response['next']);
-        }
-    }
+		if (isset($response['next'])) {
+			$this->next($response['next']);
+		}
+	}
 
-    public function __destruct()
-    {
-        curl_close($this->ch);
-        unlink(dirname(__FILE__) . '/cookie.txt');
-        unlink(dirname(__FILE__) . '/errorlog.txt');
-    }
+	public function __destruct()
+	{
+		curl_close($this->ch);
+		unlink(dirname(__FILE__) . '/cookie.txt');
+		unlink(dirname(__FILE__) . '/errorlog.txt');
+	}
 
 }

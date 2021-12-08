25b217c14073f9b175c20f809ccfc3896c369a11
Zakisu <zaki.sulistya@gmail.com>
2021-12-08 09:34:14 +0700

    Remove junk file

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-$xml = simplexml_load_file("install.xml") or die("Error: Cannot create object");
-$module_name = $xml->name;
-$module_version = $xml->version;
-echo $module_name . "\n";
-echo $module_version . "\n";
